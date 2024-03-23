92cc032600138e60116080985c8720d0bd5eb2cf
ser20 <ser20@Nitro5SerYi>
2024-03-23 12:55:32 +0100

    Inicio Total

---
+++ src/main/java/com/mycompany/proyectoactualizarrepositorio/ProyectoActualizarRepositorio.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyectoactualizarrepositorio;
+
+/**
+ *
+ * @author ser20
+ */
+public class ProyectoActualizarRepositorio {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
