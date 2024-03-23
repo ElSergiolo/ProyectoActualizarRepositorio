a2563994862ea0b39d9234b72b415d0e82c2dc53
ser20 <ser20@Nitro5SerYi>
2024-03-24 00:33:24 +0100

    Version 2.0

--- src/main/java/com/mycompany/proyectoactualizarrepositorio/ProyectoActualizarRepositorio.java
+++ src/main/java/com/mycompany/proyectoactualizarrepositorio/ProyectoActualizarRepositorio.java
@@ -4,13 +4,28 @@
 
 package com.mycompany.proyectoactualizarrepositorio;
 
+import java.util.Random;
+
 /**
  *
  * @author ser20
  */
 public class ProyectoActualizarRepositorio {
 
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz";
+    private static final int NAME_LENGTH = 5;
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Random random = new Random();
+        StringBuilder builder = new StringBuilder(NAME_LENGTH);
+
+        for (int i = 0; i < NAME_LENGTH; i++) {
+            int index = random.nextInt(ALPHABET.length());
+            char randomChar = ALPHABET.charAt(index);
+            builder.append(randomChar);
+        }
+
+        String randomName = builder.toString();
+        System.out.println("Nombre generado aleatoriamente: " + randomName);
     }
 }
